c1b4878ec179f66878c0c62cd611b2af26488109
Shakeel Ahmad <150515418+ShakeelAhmad18@users.noreply.github.com>
2024-07-15 11:44:44 +0500

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({
+    origin:["http://localhost:3000/","https://mern-task-app.onrender.com"]
+}))
 app.use("/api/task",taskRoutes)
 
 //const logger=(req,res)=>{
